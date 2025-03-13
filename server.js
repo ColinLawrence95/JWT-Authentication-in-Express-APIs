078c10e45815ebd612addde41c4c2ae48e8d14fc
Colin <colin.lawrence95@gmail.com>
2025-03-13 12:21:33 -0400

    finished page 2

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 const testJwtRouter = require('./controllers/test-jwt.js');
+const authRouter = require("./controllers/auth.js");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -16,7 +17,9 @@ mongoose.connection.on('connected', () => {
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
+app.use("/auth", authRouter);
 app.use('/test-jwt', testJwtRouter);
+
 
 // Routes go here
 
